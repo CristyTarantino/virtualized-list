30a123eba2f4bb44077f34d09e5f64fb7870f27d
tarantino <cristina@cable.tech>
2024-04-10 22:10:44 +0200

    deployment script functionality

--- app/actions/products.ts
+++ app/actions/products.ts
@@ -10,12 +10,15 @@ export async function getProducts(): Promise<GetProductResponse> {
   // forcing no cache for next js caching limitations going over 2M
   // pagination could be implemented
   // however the difficulty of this test is to load all the items and let the FE deal with the virtualization issue
-  const response = await fetch(`${process.env.HTTP_HOST}/api/products`, {
-    cache: "no-store",
-    next: {
-      tags: ["products"],
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/products`,
+    {
+      cache: "no-store",
+      next: {
+        tags: ["products"],
+      },
     },
-  });
+  );
   return response.json();
 }
 
@@ -26,10 +29,13 @@ export type AddProductResponse = {
 export async function addProduct(
   product: ProductListItem,
 ): Promise<AddProductResponse> {
-  const response = await fetch(`${process.env.HTTP_HOST}/api/products`, {
-    method: "POST",
-    body: JSON.stringify({ product }),
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/products`,
+    {
+      method: "POST",
+      body: JSON.stringify({ product }),
+    },
+  );
 
   revalidateTag("products");
   return response.json();
